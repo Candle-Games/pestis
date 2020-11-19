2e9677852efb2116aa75a32ff5882f7385f51b43
RPT0998 <43405375+RPT0998@users.noreply.github.com>
2020-11-19 18:09:38 +0100

    chapao

--- client/resources/maps/level1/wall-vines.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="wall-vines" tilewidth="16" tileheight="16" tilecount="4050" columns="90">
- <image source="images/wall-vines.png" width="1440" height="726"/>
-</tileset>
